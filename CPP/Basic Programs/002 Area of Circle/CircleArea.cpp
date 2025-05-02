e553aaeb3fc582d196be83d2f70f48b0377ecbe2
Mortaza Nabavi <43119586+MortazaNabavi@users.noreply.github.com>
2025-05-02 13:21:44 +0430

    Add files via upload

---
+++ CPP/Basic Programs/002 Area of Circle/CircleArea.cpp
@@ -0,0 +1,18 @@
+ #include <iostream>
+ #include <math.h>
+ using namespace std;
+ int main(){
+ 	
+ 	double r;
+ 	cout<<"Please Enter R: ";
+ 	cin>>r;
+ 	
+ 	double Pi=3.14;
+ 	
+ 	double area;
+ 	area=Pi*pow(r,2);
+ 	
+ 	cout<<"Area is: "<<area;
+ 	
+ 	return 0;
+ }
