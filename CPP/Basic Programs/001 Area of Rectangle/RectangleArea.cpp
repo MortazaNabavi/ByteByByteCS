287244de63f0d7ab81dc08731cbecd5fd0edb039
Mortaza Nabavi <43119586+MortazaNabavi@users.noreply.github.com>
2025-05-02 09:18:54 +0430

    Add files via upload

---
+++ CPP/Basic Programs/001 Area of Rectangle/RectangleArea.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	int h;
+	int w;
+	
+	cout<<"Enter the Hight of Rectangle: ";
+	cin>>h;
+	
+	cout<<"Enter the Width of Rectangle: ";
+	cin>>w;
+	
+	int area;
+	area=h*w;
+	
+	cout<<"the Area is: "<<area;
+	
+	return 0;
+}
